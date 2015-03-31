f96915eb19bfab39acd7cc6c3dd3d56412b4b552
4jzbra <zeraj506@yahoo.com>
2015-03-31 23:53:52 +0200

    Updated AccountTest.java

--- src/test/java/mt/edu/um/AccountTest.java
+++ src/test/java/mt/edu/um/AccountTest.java
@@ -5,6 +5,32 @@ import org.junit.Test;
 
 
 public class AccountTest {
+	private Account acc = new Account();
+	
+	@Test
+	public void setAccountNumberTest(){
+		int expected = 34;
+		acc.setAccountNumber(expected);
+		int actual = acc.getAccountNumber();
+		Assert.assertEquals(expected, actual);
+	}
+	
+	@Test
+	public void setAccountNameTest(){
+		String expected = "Fixed";
+		acc.setAccountName(expected);
+		String actual = acc.getAccountName();
+		Assert.assertEquals(expected, actual);
+	}
+	
+	@Test
+	public void setAccountBalanceTest(){
+		long expected = 25000;
+		acc.setAccountBalance(expected);
+		long actual = acc.getAccountBalance();
+		Assert.assertEquals(expected, actual);
+	}
+	
 	
 	
 
