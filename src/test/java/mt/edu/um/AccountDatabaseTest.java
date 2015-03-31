616e2de93513d6180a343628ea1e1763d3a19636
4jzbra <zeraj506@yahoo.com>
2015-04-01 01:14:43 +0200

    updating AccountDatabaseTest

--- src/test/java/mt/edu/um/AccountDatabaseTest.java
+++ src/test/java/mt/edu/um/AccountDatabaseTest.java
@@ -4,5 +4,15 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class AccountDatabaseTest {
+	
+	@Test
+	public void getAccountTest(){
+		
+	}
+	
+	@Test
+	public void getSizeTest(){
+		
+	}
 
 }
