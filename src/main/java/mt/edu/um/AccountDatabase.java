851fbb36095ed21ec85d30c7b832f794bf6e0438
Samuel <mizzisamuel@gmail.com>
2015-04-01 00:44:23 +0200

    accountdb update

--- src/main/java/mt/edu/um/AccountDatabase.java
+++ src/main/java/mt/edu/um/AccountDatabase.java
@@ -1,7 +1,11 @@
 package mt.edu.um;
 
+import java.util.ArrayList;
+
 public class AccountDatabase{
 	
+	private static AccountDatabase instance = null;	
+	ArrayList<Account> accountsArray = new ArrayList<Account>();
 	
 	public Account getAccount (int accountNumber){
 		Account acc = new Account();
