c7619f43d11218ed409f449523bdd80952e67d2d
4jzbra <zeraj506@yahoo.com>
2015-04-01 11:07:13 +0200

    updates

--- src/main/java/mt/edu/um/App.java
+++ src/main/java/mt/edu/um/App.java
@@ -1,14 +1,9 @@
 package mt.edu.um;
 
-/**
- * Hello world!
- *
- */
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!!!" );
-        System.out.println("Hello again");
+        System.out.println( "Hello World!" );
     }
 }
