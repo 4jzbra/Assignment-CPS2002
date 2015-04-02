a38809687b37c296f39dd236409c7a75be4d4716
Samuel <mizzisamuel@gmail.com>
2015-04-02 17:00:30 +0200

    AccountTest revised

--- src/test/java/mt/edu/um/AccountTest.java
+++ src/test/java/mt/edu/um/AccountTest.java
@@ -10,13 +10,13 @@ public class AccountTest {
 	@Test
 	public void adjustBalanceTest1(){
 		final Account acc1 = new Account(1, "Savings", 15000);
-		Assert.assertEquals(true, acc1.adjustBalance(-5000));
+		Assert.assertEquals(true, acc1.adjustBalance(-5000));  // new balance > 0 therefore true
 	}
 	
 	@Test
 	public void adjustBalanceTest2(){
 		final Account acc1 = new Account(1, "Savings", 15000);
-		Assert.assertEquals(false, acc1.adjustBalance(-25000));
+		Assert.assertEquals(false, acc1.adjustBalance(-25000)); // new balance < 0 therefore false
 	}
 	
 	
@@ -53,7 +53,7 @@ public class AccountTest {
 	}
 	
 	@Test
-	public void getAccountName(){
+	public void getAccountNameTest(){
 		String expected = "Savings";
 		acc.setAccountName(expected);
 		String actual = acc.getAccountName();
@@ -61,11 +61,27 @@ public class AccountTest {
 	}
 	
 	@Test
-	public void getAccountBalance(){
+	public void getAccountBalanceTest(){
 		long expected = 10000;
 		acc.setAccountBalance(expected);
 		long actual = acc.getAccountBalance();
 		Assert.assertEquals(expected, actual);
+	}
+	
+	@Test
+	public void printAccountDetailsTest(){    // (provisional test)
+		int expected1 = 414;
+		String expected2 = "Savings";
+		long expected3 = 10000;
+		acc.setAccountNumber(expected1);
+		acc.setAccountName(expected2);
+		acc.setAccountBalance(expected3);
+		int actual1 = acc.getAccountNumber();
+		String actual2 = acc.getAccountName();
+		long actual3 = acc.getAccountBalance();
+		Assert.assertEquals(expected1, actual1);
+		Assert.assertEquals(expected2, actual2);
+		Assert.assertEquals(expected3, actual3);
 	}
 	
 	
