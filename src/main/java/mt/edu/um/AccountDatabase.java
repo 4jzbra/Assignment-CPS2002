f5d5828eba69b6d10b11326d3fc17e2caa3abf74
4jzbra <zeraj506@yahoo.com>
2015-04-02 13:08:06 +0200

    added getInstance() method
    This returns the only instance of this class. If the instance in not yet
    initialised, it is initialised and returned.

--- src/main/java/mt/edu/um/AccountDatabase.java
+++ src/main/java/mt/edu/um/AccountDatabase.java
@@ -2,10 +2,18 @@ package mt.edu.um;
 
 import java.util.ArrayList;
 
+//Singleton Class
 public class AccountDatabase{
 	
 	private static AccountDatabase instance = null;	
 	ArrayList<Account> accountsArray = new ArrayList<Account>();
+	
+	public static AccountDatabase getInstance(){ //Lazy instantiation of the singleton object
+		if(instance == null){
+			instance = new AccountDatabase();
+		}
+		return instance;
+	}
 	
 	public Account getAccount(int accountNumber){
 		for(int i = 0; i < this.accountsArray.size(); i++){
