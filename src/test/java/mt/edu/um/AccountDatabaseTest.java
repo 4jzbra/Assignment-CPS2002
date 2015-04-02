1ba3809e61f61fc1e7016565586d350bbd0ea422
4jzbra <zeraj506@yahoo.com>
2015-04-02 13:31:47 +0200

    updating AccountDatabase and it's tests
    created the singletonTest() method.
    Here two instances of AccountDatabase were created, db1 and db2.
    Two Accounts were created and one was added to the instance db1, and the
    other to db2.
    Lastly it was checked whether both instances (i.e. db1 and db2) are
    equal.

--- src/test/java/mt/edu/um/AccountDatabaseTest.java
+++ src/test/java/mt/edu/um/AccountDatabaseTest.java
@@ -5,7 +5,6 @@ import org.junit.Test;
 
 public class AccountDatabaseTest {
 	
-	private static AccountDatabase singleton = new AccountDatabase();
 	
 	@Test
 	public void getAccountTest(){
@@ -14,9 +13,24 @@ public class AccountDatabaseTest {
 	
 	@Test
 	public void getSizeTest(){
+		final AccountDatabase singleton = new AccountDatabase();
 		int actual = singleton.accountsArray.size();
 		int expected = singleton.getSize();
 		Assert.assertEquals(expected, actual);	
+	}
+	
+	@Test
+	public void singletonTest(){
+		final AccountDatabase db1, db2;
+		db1 = AccountDatabase.getInstance();
+		db2 = AccountDatabase.getInstance();
+		final Account acc1 = new Account(5, "Fixed", 12000);
+		final Account acc2 = new Account(6, "Savings", 34000);
+		db1.addNewAccount(acc1);
+		db2.addNewAccount(acc2);
+		
+		Assert.assertEquals(true, db1 == db2);
+		
 	}
 
 }
