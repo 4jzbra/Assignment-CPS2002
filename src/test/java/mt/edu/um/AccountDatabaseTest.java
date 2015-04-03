6d7582a58cf868cfd9b30204b7d7024c6fa9e377
4jzbra <zeraj506@yahoo.com>
2015-04-03 23:12:38 +0200

    new methods

--- src/test/java/mt/edu/um/AccountDatabaseTest.java
+++ src/test/java/mt/edu/um/AccountDatabaseTest.java
@@ -19,6 +19,26 @@ public class AccountDatabaseTest {
 		int expected = database.getSize();
 		Assert.assertEquals(expected, actual);	
 	}
+	
+	@Test
+	public void addNewAccountTest1() {
+		final Account acc = new Account(1, "Savings", 2000);
+		Assert.assertEquals(true, database.addNewAccount(acc));
+	}
+	
+	@Test
+	public void addNewAccountTest2(){
+		Account acc1 = new Account(1, "Fixed", 5600);
+		Account acc2 = new Account(1, "Savings", 89000);
+		database.addNewAccount(acc1);
+		Assert.assertEquals(false, database.addNewAccount(acc2));
+	}
+	
+	public void alreadyExistsTest(){
+		Account newAcc = new Account(1, "Fixed", 5600);
+		database.accountsArray.add(newAcc);
+		Assert.assertEquals(true, database.alreadyExists(1));	
+	}
 
 
 }
