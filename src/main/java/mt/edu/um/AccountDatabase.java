93f6f05687e0c18624eaebe5d5bdada86ac39966
4jzbra <zeraj506@yahoo.com>
2015-04-03 22:38:06 +0200

    more edits

--- src/main/java/mt/edu/um/AccountDatabase.java
+++ src/main/java/mt/edu/um/AccountDatabase.java
@@ -22,24 +22,5 @@ public class AccountDatabase{
 		return this.accountsArray.size();
 	}
 	
-	//method to add new account to the ArrayList
-	public boolean addNewAccount(Account acc){
-		if (this.alreadyExists(acc.getAccountNumber()) == true) {
-			System.out.println("Error: Account number "+acc.getAccountNumber()+" already exists!");
-			return false;
-		} else {
-			this.accountsArray.add(acc);
-			return true;
-		}
-	}
-	
-	public boolean alreadyExists(int accNo){
-		for(int i = 0; i < this.getSize(); i++){
-			if(accNo == accountsArray.get(i).getAccountNumber()){
-				return true;
-			}
-		}
-		return false;
-	}
 
 }
