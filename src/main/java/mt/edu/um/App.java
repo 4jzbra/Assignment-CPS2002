11920513c383337ba85bc8c9370546abe5a83219
Samuel <mizzisamuel@gmail.com>
2015-04-06 10:19:57 +0200

    extra check using app

--- src/main/java/mt/edu/um/App.java
+++ src/main/java/mt/edu/um/App.java
@@ -8,10 +8,11 @@ public class App
     {
     	Account acc1 = new Account(1, "Fixed", 1000);
 		Account acc2 = new Account(2, "Savings", 2000);
-		Account acc3= new Account(3, "Savings", 2500);
+		Account acc3 = new Account(3, "Savings", 2500);
 		Account acc4 = new Account(4, "Fixed", 4000);
 		Account acc5 = new Account(2, "Fixed", 6700);
 		Account acc6 = new Account(4, "Fixed", 4900);
+		Account acc7 = new Account(5, "Visa", -500);
 		
 		database.addNewAccount(acc1);
 		database.addNewAccount(acc2);
@@ -19,7 +20,12 @@ public class App
 		database.addNewAccount(acc4);
 		database.addNewAccount(acc5);
 		database.addNewAccount(acc6);
-		System.out.println(database.getSize());
+		database.addNewAccount(acc7);
+		
+		acc7.adjustBalance(500);  // settles account to 0
+		
+		System.out.println("Accounts in database: " + database.getSize());
+		
 		printAllAccounts();
     }
     
