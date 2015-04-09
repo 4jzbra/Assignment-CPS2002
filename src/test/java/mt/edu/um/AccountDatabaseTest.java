731aa0ac1db614c59737edb1c94d89db8c289f57
4jzbra <zeraj506@yahoo.com>
2015-04-09 13:08:03 +0200

    Edited tests in AccountDatabaseTest

--- src/test/java/mt/edu/um/AccountDatabaseTest.java
+++ src/test/java/mt/edu/um/AccountDatabaseTest.java
@@ -8,17 +8,36 @@ public class AccountDatabaseTest {
 	final AccountDatabase database = new AccountDatabase();
 	
 	@Test
-	public void getAccountTest(){
-		Account acc = new Account(4, "Fixed", 1000);
+	public void initialEmptyDatabase(){
+		Assert.assertEquals(true, AccountDatabase.accountsArray.isEmpty());
+	}
+	
+	@Test
+	public void getAccountTest1(){
+		Account acc = new Account(1, "Fixed", 1000);
 		database.addNewAccount(acc);
 		Assert.assertEquals(acc, AccountDatabase.accountsArray.get(database.getSize()-1));
 	}
 	
 	@Test
-	public void getSizeTest(){
-		int actual = AccountDatabase.accountsArray.size();
-		int expected = database.getSize();
-		Assert.assertEquals(expected, actual);	
+	public void getAccountTest2(){
+		Account acc = new Account(2, "Savings", 4000);
+		database.addNewAccount(acc);
+		Assert.assertEquals(acc, AccountDatabase.getAccount(2));
+	}
+	
+	@Test
+	public void getSizeTest1(){
+		int expected = AccountDatabase.accountsArray.size();
+		Assert.assertEquals(expected, database.getSize());	
+	}
+	
+	@Test
+	public void getSizeTest2(){
+		Account acc = new Account(3, "Savings", 3000);
+		database.addNewAccount(acc);
+		int expected = AccountDatabase.accountsArray.size();
+		Assert.assertEquals(expected, database.getSize());	
 	}
 	
 	@Test  // when account is unique
@@ -29,22 +48,24 @@ public class AccountDatabaseTest {
 	
 	@Test  // when account already exists
 	public void addNewAccountTest2(){
+		AccountDatabase.accountsArray.clear();
 		Account acc1 = new Account(1, "Fixed", 5600);
 		Account acc2 = new Account(1, "Savings", 89000);
 		database.addNewAccount(acc1);
 		Assert.assertEquals(false, database.addNewAccount(acc2));
 	}
 	
-	@Test  // when account already exists
+	@Test  // checking if account already exists
 	public void alreadyExistsTest1(){
+		AccountDatabase.accountsArray.clear();
 		Account newAcc = new Account(1, "Fixed", 5600);
 		AccountDatabase.accountsArray.add(newAcc);
 		Assert.assertEquals(true, database.alreadyExists(1));	
 	}
 	
-	@Test  // when account is unique
+	@Test  // when account does not exist
 	public void alreadyExistsTest2(){
-		Account newAcc = new Account(1, "Fixed", 5600);
+		Account newAcc = new Account(6, "Fixed", 5600);
 		AccountDatabase.accountsArray.add(newAcc);
 		Assert.assertEquals(false, database.alreadyExists(5));	
 	}  
