71b6bf596e6d71a57b5a79919dfb2b704cfa4c6e
4jzbra <zeraj506@yahoo.com>
2015-04-09 12:43:11 +0200

    Added more tests in AccountTest

--- src/test/java/mt/edu/um/AccountTest.java
+++ src/test/java/mt/edu/um/AccountTest.java
@@ -20,54 +20,58 @@ public class AccountTest {
 	}
 	
 	@Test
+	public void adjustBalanceTest3(){
+		final Account acc1 = new Account(1, "Savings", 15000);
+		Assert.assertEquals(true, acc1.adjustBalance(-15000)); // new balance = 0 therefore true
+	}
+	
+	@Test
+	public void adjustBalanceTest4(){
+		final Account acc1 = new Account(1, "Savings", 15000);
+		acc1.adjustBalance(5000);
+		Assert.assertEquals(20000, acc1.getAccountBalance());
+	}
+	
+	@Test
 	public void setAccountNumberTest(){
 		int expected = 34;
 		acc.setAccountNumber(expected);
-		int actual = acc.getAccountNumber();
-		Assert.assertEquals(expected, actual);
+		Assert.assertEquals(expected, acc.getAccountNumber());
 	}
 	
 	@Test
 	public void setAccountNameTest(){
 		String expected = "Fixed";
 		acc.setAccountName(expected);
-		String actual = acc.getAccountName();
-		Assert.assertEquals(expected, actual);
+		Assert.assertEquals(expected, acc.getAccountName());
 	}
 	
 	@Test
 	public void setAccountBalanceTest(){
 		long expected = 25000;
 		acc.setAccountBalance(expected);
-		long actual = acc.getAccountBalance();
-		Assert.assertEquals(expected, actual);
+		Assert.assertEquals(expected, acc.getAccountBalance());
 	}
 	
 	@Test
 	public void getAccountNumberTest(){
         int expected = 414;
         acc.setAccountNumber(expected);
-        int actual = acc.getAccountNumber();
-        Assert.assertEquals(expected, actual);       
+        Assert.assertEquals(expected, acc.getAccountNumber());       
 	}
 	
 	@Test
 	public void getAccountNameTest(){
 		String expected = "Savings";
 		acc.setAccountName(expected);
-		String actual = acc.getAccountName();
-		Assert.assertEquals(expected, actual);
+		Assert.assertEquals(expected, acc.getAccountName());
 	}
 	
 	@Test
 	public void getAccountBalanceTest(){
 		long expected = 10000;
 		acc.setAccountBalance(expected);
-		long actual = acc.getAccountBalance();
-		Assert.assertEquals(expected, actual);
+		Assert.assertEquals(expected, acc.getAccountBalance());
 	}
-	
-
-	
 
 }
