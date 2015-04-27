a30bc504e18a937f49ea050d74a7385201e3440e
Samuel <mizzisamuel@gmail.com>
2015-04-28 00:41:35 +0200

    atomic and compound
    composite design pattern?

--- src/main/java/mt/edu/um/AtomicTransaction.java
+++ src/main/java/mt/edu/um/AtomicTransaction.java
@@ -1,5 +1,10 @@
 package mt.edu.um;
 
-public class AtomicTransaction {
-
+public class AtomicTransaction {   // the leaf of the composite design pattern
+		
+	private Transaction leaf;  // the actual transaction (with src, dst...)
+	
+	public AtomicTransaction(){
+		
+	}
 }
--- src/main/java/mt/edu/um/CompoundTransaction.java
+++ src/main/java/mt/edu/um/CompoundTransaction.java
@@ -1,9 +1,30 @@
 package mt.edu.um;
 
+import java.util.ArrayList;
+
 public class CompoundTransaction {
+	// using composite design pattern - tree of transactions
+	
+	private String name;   // name of compound transaction
+	
+	private ArrayList<Transaction> childTrans =  new ArrayList<Transaction>();///
+	
 	
 	public CompoundTransaction(){
 		
+	}
+	
+	public CompoundTransaction(String n){
+		setName(n);
+	}
+	
+	
+	public void setName(String n){
+		name = n;
+	}
+	
+	public String getName(){
+		return name;
 	}
 
 }
