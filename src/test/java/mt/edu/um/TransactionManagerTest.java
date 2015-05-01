5dfa0271c26541bc5b712e3661a2c956728e9815
Samuel <mizzisamuel@gmail.com>
2015-05-01 15:37:40 +0200

    more tests

--- src/test/java/mt/edu/um/TransactionManagerTest.java
+++ src/test/java/mt/edu/um/TransactionManagerTest.java
@@ -7,16 +7,19 @@ import org.junit.Test;
 public class TransactionManagerTest {
 	private TransactionManager transactionM;
 	private AccountDatabase database;
-	
+	private CompoundTransaction compoundT;
+
 	
 	@Before
-	public void initialize(){
+	public void initialize() {
 		transactionM = new TransactionManager();
 		database = new AccountDatabase();
+		compoundT = new CompoundTransaction("Main Transaction");
 	}
 
+	// Tests for Atomic processTransaction
 	@Test
-	public void processTransactionTest1() {
+	public void processTransactionTest1() {             
 		final Account acc1 = new Account(1, "Savings", 5000);
 		final Account acc2 = new Account(2, "Savings", 3500);
 		database.addNewAccount(acc1);
@@ -24,7 +27,7 @@ public class TransactionManagerTest {
 		Assert.assertEquals(true, transactionM.processTransaction(1, 2, 3000));
 	}
 
-	@Test (expected = IllegalArgumentException.class)  // insufficient balance
+	@Test (expected = IllegalArgumentException.class)  // insufficient balance 
 	public void processTransactionTest2() {
 		final Account acc1 = new Account(3, "Savings", 5000);
 		final Account acc2 = new Account(4, "Savings", 3500);
@@ -33,10 +36,31 @@ public class TransactionManagerTest {
 		transactionM.processTransaction(3, 4, 6000);
 	}
 
-	@Test (expected = NullPointerException.class) // accounts do not exist test
-	public void processTransactionTest5() {
+	@Test (expected = NullPointerException.class) // accounts do not exist test 
+	public void processTransactionTest3() {
 		transactionM.processTransaction(15, 16, 2000);
 	}
+	
+	//Tests for Compound processTransaction
+	@Test
+	public void processTransactionTest4() {
+		final Account acc1 = new Account(3, "Savings", 5000);
+		final Account acc2 = new Account(4, "Savings", 3500);
+		database.addNewAccount(acc1);
+		database.addNewAccount(acc2);
+		Transaction t1 = new AtomicTransaction(3,4,300);
+    	Transaction t2 = new AtomicTransaction(4,3,400);
+    	compoundT.addTransaction(t1);
+    	compoundT.addTransaction(t2);
+    	Assert.assertEquals(true, transactionM.processTransaction(compoundT));
+	}
+	
+	@Test (expected = IllegalArgumentException.class) // compound transaction with no elements
+	public void processTransactionTest5() {
+		transactionM.processTransaction(compoundT);
+	}
+	
+	
 	
 	@Test  //testing constructor for AtomicTransaction
 	public void constructorTest1() {
