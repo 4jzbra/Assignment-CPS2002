4d21c3b82e41aa6348b05a1ac637f79db6691cc9
4jzbra <zeraj506@yahoo.com>
2015-05-01 13:47:10 +0200

    more test cases

--- src/test/java/mt/edu/um/CompoundTransactionTest.java
+++ src/test/java/mt/edu/um/CompoundTransactionTest.java
@@ -16,18 +16,39 @@ public class CompoundTransactionTest {
 		acc1 = new Account(1, "Bank Loan", 10000);
 		acc2 = new Account(2, "Payee Current Account", 4500);
 		acc3 = new Account(3, "Seller Current Account", 10000);
+		database.addNewAccount(acc1);
+		database.addNewAccount(acc2);
+		database.addNewAccount(acc3);
 	}
 	
 	@Test 
 	public void processTest1() {
-		database.addNewAccount(acc1);
-		database.addNewAccount(acc2);
-		database.addNewAccount(acc3);
 		transaction = new CompoundTransaction("Prepare Bank Loan");
 		transaction.addTransaction(new AtomicTransaction(1,2, 5000));
 		transaction.addTransaction(new AtomicTransaction(2,3, 8000));
 		tm = new TransactionManager();
 		Assert.assertEquals(true, tm.processTransaction(transaction));
+	}
+	
+	@Test (expected = IllegalArgumentException.class)
+	public void processTest2() {
+		transaction = new CompoundTransaction("Prepare Bank Loan");
+		tm = new TransactionManager();
+		tm.processTransaction(transaction);
+	}
+	
+	@Test
+	public void addTransactionTest(){
+		transaction = new CompoundTransaction();
+		transaction.addTransaction(new AtomicTransaction(1,2,100));
+		Assert.assertEquals(1, transaction.getElements().size());
+	}
+	
+	@Test
+	public void setNameTest(){
+		transaction = new CompoundTransaction();
+		transaction.setName("Pay Loan");
+		Assert.assertEquals("Pay Loan", transaction.getName());
 	}
 
 }
--- src/test/java/mt/edu/um/TransactionFactoryTest.java
+++ src/test/java/mt/edu/um/TransactionFactoryTest.java
@@ -31,5 +31,4 @@ public class TransactionFactoryTest {
 		Assert.assertEquals(null, factory.getTransaction("abc"));
 	}
 	
-	
 }
