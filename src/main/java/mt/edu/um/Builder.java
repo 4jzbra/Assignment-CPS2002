69d00721a0218430a35465365e89b6adb012848b
4jzbra <zeraj506@yahoo.com>
2015-05-02 16:38:38 +0200

    abstract class methods

--- src/main/java/mt/edu/um/Builder.java
+++ src/main/java/mt/edu/um/Builder.java
@@ -1,5 +1,18 @@
 package mt.edu.um;
 
 public abstract class Builder {
-
+	
+	static Builder getRiskBuilder(String risk){
+		
+		//need to implement
+		return null;
+	}
+	
+	public void buildDepost(){};
+	
+	public void buildMainTransaction(){};
+	
+	public void buildCommision(){};
+	
+	
 }
