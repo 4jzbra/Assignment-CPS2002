3ad0939db780339e532366e96e2df64212cb5728
4jzbra <zeraj506@yahoo.com>
2015-05-02 16:48:34 +0200

    highRiskBuilder

--- src/main/java/mt/edu/um/HighRiskBuilder.java
+++ src/main/java/mt/edu/um/HighRiskBuilder.java
@@ -1,5 +1,24 @@
 package mt.edu.um;
 
-public class HighRiskBuilder {
+public class HighRiskBuilder extends Builder {
+	
+	private Transaction compoundTransaction = new CompoundTransaction(); //not sure if we need this
+	
+	public HighRiskBuilder(){
+		
+	}
+	
+	public void buildDepost(){
+		AtomicTransaction deposit = new AtomicTransaction();
+		deposit.setSourceAccountNumber(3123); //we need to create an account for this somewhere in the program
+	};
+	
+	public void buildMainTransaction(){};
+	
+	public void buildCommision(){};
+	
+	public Transaction getWholeTransaction(){
+		return compoundTransaction;
+	}
 
 }
