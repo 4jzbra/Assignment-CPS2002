c50a9b1ca7cebf0f9e2c483aca08a56e82378fa2
Samuel <mizzisamuel@gmail.com>
2015-05-19 10:54:13 +0200

    printtransactions update

--- src/main/java/iterator/PrintTransactions.java
+++ src/main/java/iterator/PrintTransactions.java
@@ -7,6 +7,9 @@ import mt.edu.um.AtomicTransaction;
 public class PrintTransactions {
 			
 	boolean printTransactions(ArrayList<AtomicTransaction> array){
+		
+		if (array == null)  return false;
+		
 		for(AtomicTransaction atomicTrans : array){
 			System.out.print("Source: " + atomicTrans.getSourceAccountNumber());
 			System.out.print("   Destination: "+ atomicTrans.getDestinationAccountNumber());
